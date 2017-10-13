cc6af43c80dff9a63855bbb8b1421189b3505576
chilingirov <vasko988@gmail.com>
2017-10-13 03:57:41 -0700

    Fixed a bug

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var Enemy = function(x, y) {
     this.x = x;
     this.y = y;
     this.sprite = 'images/enemy-bug.png';
-    this.speed = Math.random() * 200 + 1;
+    this.speed = Math.random() * (400 - 100) + 100;
 };
 
 // Update the enemy's position, multiply the position with dt
